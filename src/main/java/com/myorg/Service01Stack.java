8e9ba667f1796dca0944d4407b7fdc9a09370068
suleiman-moraes <suleimanmoraes@gmail.com>
2021-06-11 07:22:50 -0300

    Update service 01

--- src/main/java/com/myorg/Service01Stack.java
+++ src/main/java/com/myorg/Service01Stack.java
@@ -46,7 +46,7 @@ public class Service01Stack extends Stack {
 		ApplicationLoadBalancedFargateService service01 = ApplicationLoadBalancedFargateService.Builder
 				.create(this, "ALB01").serviceName("service-01").cluster(cluster).cpu(512).memoryLimitMiB(1024).desiredCount(2).listenerPort(8080)
 				.taskImageOptions(ApplicationLoadBalancedTaskImageOptions.builder().containerName("aws_project01")
-						.image(ContainerImage.fromRegistry("suleimanmoaraes/curso_aws_project01:1.5.1"))
+						.image(ContainerImage.fromRegistry("suleimanmoaraes/curso_aws_project01:1.5.3"))
 						.containerPort(8080)
 						.logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder()
 								.logGroup(LogGroup.Builder.create(this, "Service01LogGroup").logGroupName("Service01")
