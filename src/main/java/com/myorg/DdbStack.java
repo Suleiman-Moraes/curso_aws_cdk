f18f11a0b5da5bebe49882c2fdba9448f4dc6b40
suleiman-moraes <suleimanmoraes@gmail.com>
2021-06-15 07:49:55 -0300

    Final para local stack

--- src/main/java/com/myorg/DdbStack.java
+++ src/main/java/com/myorg/DdbStack.java
@@ -1,43 +1,39 @@
 package com.myorg;
 
 import software.amazon.awscdk.core.Construct;
+import software.amazon.awscdk.core.Duration;
 import software.amazon.awscdk.core.RemovalPolicy;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
 import software.amazon.awscdk.services.dynamodb.Attribute;
 import software.amazon.awscdk.services.dynamodb.AttributeType;
 import software.amazon.awscdk.services.dynamodb.BillingMode;
+import software.amazon.awscdk.services.dynamodb.EnableScalingProps;
 import software.amazon.awscdk.services.dynamodb.Table;
+import software.amazon.awscdk.services.dynamodb.UtilizationScalingProps;
 
 public class DdbStack extends Stack {
-    private final Table productEventsDdb;
+	private final Table productEventsDdb;
 
-    public DdbStack(final Construct scope, final String id) {
-        this(scope, id, null);
-    }
+	public DdbStack(final Construct scope, final String id) {
+		this(scope, id, null);
+	}
 
-    public DdbStack(final Construct scope, final String id, final StackProps props) {
-        super(scope, id, props);
+	public DdbStack(final Construct scope, final String id, final StackProps props) {
+		super(scope, id, props);
 
-        productEventsDdb = Table.Builder.create(this, "ProductEventsDb")
-                .tableName("product-events")
-                .billingMode(BillingMode.PROVISIONED)
-                .readCapacity(1)
-                .writeCapacity(1)
-                .partitionKey(Attribute.builder()
-                        .name("pk")
-                        .type(AttributeType.STRING)
-                        .build())
-                .sortKey(Attribute.builder()
-                        .name("sk")
-                        .type(AttributeType.STRING)
-                        .build())
-                .timeToLiveAttribute("ttl")
-                .removalPolicy(RemovalPolicy.DESTROY)
-                .build();
-    }
+		productEventsDdb = Table.Builder.create(this, "ProductEventsDb").tableName("product-events")
+				.billingMode(BillingMode.PROVISIONED).readCapacity(1).writeCapacity(1)
+				.partitionKey(Attribute.builder().name("pk").type(AttributeType.STRING).build())
+				.sortKey(Attribute.builder().name("sk").type(AttributeType.STRING).build()).timeToLiveAttribute("ttl")
+				.removalPolicy(RemovalPolicy.DESTROY).build();
 
-    public Table getProductEventsDdb() {
-        return productEventsDdb;
-    }
+		productEventsDdb.autoScaleReadCapacity(EnableScalingProps.builder().minCapacity(1).maxCapacity(2).build())
+				.scaleOnUtilization(UtilizationScalingProps.builder().targetUtilizationPercent(50)
+						.scaleInCooldown(Duration.seconds(30)).scaleOutCooldown(Duration.seconds(30)).build());
+	}
+
+	public Table getProductEventsDdb() {
+		return productEventsDdb;
+	}
 }
--- src/main/java/com/myorg/Service01Stack.java
+++ src/main/java/com/myorg/Service01Stack.java
@@ -23,14 +23,17 @@ import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskI
 import software.amazon.awscdk.services.elasticloadbalancingv2.HealthCheck;
 import software.amazon.awscdk.services.events.targets.SnsTopic;
 import software.amazon.awscdk.services.logs.LogGroup;
+import software.amazon.awscdk.services.s3.Bucket;
+import software.amazon.awscdk.services.sqs.Queue;
 
 public class Service01Stack extends Stack {
-	public Service01Stack(final Construct scope, final String id, Cluster cluster, SnsTopic productEventsTopic) {
-		this(scope, id, null, cluster, productEventsTopic);
+	public Service01Stack(final Construct scope, final String id, Cluster cluster, SnsTopic productEventsTopic,
+			Bucket invoiceBucket, Queue invoiceQueue) {
+		this(scope, id, null, cluster, productEventsTopic, invoiceBucket, invoiceQueue);
 	}
 
 	public Service01Stack(final Construct scope, final String id, final StackProps props, Cluster cluster,
-			SnsTopic productEventsTopic) {
+			SnsTopic productEventsTopic, Bucket invoiceBucket, Queue invoiceQueue) {
 		super(scope, id, props);
 
 		Map<String, String> envVariables = new HashMap<>();
@@ -39,14 +42,17 @@ public class Service01Stack extends Stack {
 		envVariables.put("SPRING_DATASOURCE_USERNAME", ConstantUtil.RDS_USERNAME);
 		envVariables.put("SPRING_DATASOURCE_PASSWORD", Fn.importValue(ConstantUtil.RDS_PASSWORD));
 
+		envVariables.put("MORAES_AWS_S3-BUCKET-INVOICE-NAME", invoiceBucket.getBucketName());
+		envVariables.put("MORAES_AWS_SQS-QUEUE-INVOICE-EVENTS-NAME", invoiceQueue.getQueueName());
 		envVariables.put("MORAES_AWS_REGION", "us-east-1");
 		envVariables.put("MORAES_AWS_SNS-TOPIC-PRODUCT-EVENTS-ARN", productEventsTopic.getTopic().getTopicArn());
 		envVariables.put("MORAES_SWAGGER_SHOW", "true");
 
 		ApplicationLoadBalancedFargateService service01 = ApplicationLoadBalancedFargateService.Builder
-				.create(this, "ALB01").serviceName("service-01").cluster(cluster).cpu(512).memoryLimitMiB(1024).desiredCount(2).listenerPort(8080)
+				.create(this, "ALB01").serviceName("service-01").cluster(cluster).cpu(512).memoryLimitMiB(1024)
+				.desiredCount(2).listenerPort(8080)
 				.taskImageOptions(ApplicationLoadBalancedTaskImageOptions.builder().containerName("aws_project01")
-						.image(ContainerImage.fromRegistry("suleimanmoaraes/curso_aws_project01:1.5.3"))
+						.image(ContainerImage.fromRegistry("suleimanmoaraes/curso_aws_project01:1.7.1"))
 						.containerPort(8080)
 						.logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder()
 								.logGroup(LogGroup.Builder.create(this, "Service01LogGroup").logGroupName("Service01")
@@ -65,5 +71,8 @@ public class Service01Stack extends Stack {
 						.scaleOutCooldown(Duration.seconds(60)).build());
 
 		productEventsTopic.getTopic().grantPublish(service01.getTaskDefinition().getTaskRole());
+		
+		invoiceQueue.grantConsumeMessages(service01.getTaskDefinition().getTaskRole());
+		invoiceBucket.grantReadWrite(service01.getTaskDefinition().getTaskRole());
 	}
 }
